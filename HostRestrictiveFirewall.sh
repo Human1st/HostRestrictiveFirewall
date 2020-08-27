d5578242ce35e2e3a5dff61a9162806da9edee8a
BuildTools <unconfigured@null.spigotmc.org>
2020-08-27 10:39:29 +0200

    Adding for loop into arguments

--- HostRestrictiveFirewall.sh
+++ HostRestrictiveFirewall.sh
@@ -1,9 +1,11 @@
-#whitelistEntry($ip, $port)
-whitelistEntry() {
-  iptables -I INPUT -p tcp -s $1 --dport $2 -j ACCEPT
+#whitelistEntry $port
+# Replace <localhost> by your host name if you want
+# Or add a similar line with your host in place to whitelist multiple hosts
+whitelistLocalEntry() {
+  iptables -I INPUT -p tcp -s localhost --dport $1 -j ACCEPT
 }
 
-#blacklistEntry($port)
+#blacklistEntry $port
 blacklistEntry() {
   iptables -A INPUT -p tcp --dport $1 -j DROP
 }
@@ -12,3 +14,10 @@ resetFirewall() {
   iptables -F
   iptables -X
 }
+
+resetFirewall
+for port in "$@"
+do
+    whitelistLocalEntry $port
+    blacklistEntry $port
+done
